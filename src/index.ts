f8dd1c049c89d03b9990eef6132e44aa7d9bfe3a
HerainYayo <yangzhonghao001211@gmail.com>
2024-11-10 17:22:52 +1100

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,34 @@
 // src/index.ts
-import express from 'express';
+import express from "express";
+// import {googleapis} from "googleapis"
+import helmet from "helmet";
+import cors from "cors";
+import axios from "axios";
+import session from 'express-session';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import {moduleManagerInstance} from "./core/moduleManager";
+
+import { register as register_auth } from './auth/auth_general';
+import { register as register_broadcast } from './broadcastRoom/room_general';
+
+
+
+
+console.log(process.env);
 
 const app: express.Application = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(helmet());
+app.use(cors({
+  origin: process.env.ORIGIN,
+  credentials: true
+}));
 
 app.use(express.text());
 
@@ -10,32 +36,18 @@ app.listen(port, () => {
   console.log(`server is listening on ${port}`);
 });
 
+app.use(session({
+  secret: 'mySecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+register_auth(app, moduleManagerInstance);
+
+register_broadcast(app, moduleManagerInstance);
+
 // Homepage
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.status(200).send("Hello World!");
-});
-
-// GET
-app.get('/get', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-get-header", "get-header-value").send("get-response-from-compute");
-});
-
-//POST
-app.post('/post', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-post-header", "post-header-value").send(req.body.toString());
-});
-
-//PUT
-app.put('/put', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-put-header", "put-header-value").send(req.body.toString());
-});
-
-//PATCH
-app.patch('/patch', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-patch-header", "patch-header-value").send(req.body.toString());
-});
-
-// Delete
-app.delete('/delete', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-delete-header", "delete-header-value").send();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
